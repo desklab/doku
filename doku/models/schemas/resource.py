f6771398df30f520fe8c9e836f21a61919cf5249
ax-sc <a.schlindwein@desk-lab.de>
2020-06-26 20:04:15 +0200

    add delete method for resource-file

--- doku/models/schemas/resource.py
+++ doku/models/schemas/resource.py
@@ -1,9 +1,13 @@
 from marshmallow_sqlalchemy import auto_field
 from marshmallow import fields
+from flask import jsonify, current_app
+import os
 
 from doku.models import DateSchemaMixin
 from doku.models.resource import Resource
 from doku.models.schemas.common import DokuSchema, ApiSchemaMixin
+from doku.utils.db import get_or_404
+from doku import db
 
 
 class ResourceSchema(DokuSchema, DateSchemaMixin, ApiSchemaMixin):
@@ -17,3 +21,16 @@ class ResourceSchema(DokuSchema, DateSchemaMixin, ApiSchemaMixin):
     name = auto_field()
     filename = auto_field()
     url = fields.String(dump_only=True)
+
+    @classmethod
+    def delete(cls, instance_id: int, commit=True):
+        instance = get_or_404(
+            db.session.query(cls.Meta.model).filter_by(id=instance_id)
+        )
+        filename = instance.filename
+        os.remove(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+        db.session.delete(instance)
+        if commit:
+            db.session.commit()
+            
+        return jsonify({"success": True})
