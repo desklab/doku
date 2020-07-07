625f5733972760be0d7e355cb7c300bd5a8fd899
Jonas Drotleff <j.drotleff@desk-lab.de>
2020-07-07 10:43:46 +0200

    Create API Tokens

--- doku/blueprints/api/v1/__init__.py
+++ doku/blueprints/api/v1/__init__.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, jsonify
+from werkzeug.exceptions import BadRequest, Unauthorized
+from flask import Blueprint, jsonify, request, session
 
+from doku.models.user import User
+from doku.utils.middlewares.csrf import csrf
 
 bp = Blueprint("api.v1", __name__)
 
@@ -7,3 +10,28 @@ bp = Blueprint("api.v1", __name__)
 @bp.route("/heartbeat", methods=["GET"])
 def dashboard():
     return jsonify({"status": "healthy"})
+
+
+@bp.route("/login", methods=["POST"])
+@csrf.exempt
+def login():
+    if request.authorization is None:
+        raise BadRequest("No authorization provided")
+    email = request.authorization.get("username")
+    password = request.authorization.get("password")
+    user = User.authenticate(email=email, password=password)
+    if user is None:
+        raise Unauthorized("E-Mail and password do not match. Please try again")
+    else:
+        session.authenticated = True
+        session.update({
+            "user": user.username,
+            "id": user.id,
+            "name": user.name
+        })
+        return jsonify({
+            "success": True,
+            "token_type": "Bearer",
+            "access_token": user.token.get("token"),
+            "expires_in": user.token.get("expires_in")
+        })
