e7c99d0ab847d023fcf8e56517ff8e3ce4fb1410
Jonas Drotleff <j.drotleff@desk-lab.de>
2020-08-14 20:20:51 +0200

    Settings for postgres in dev env

--- config/dev.py
+++ config/dev.py
@@ -12,3 +12,13 @@ os.environ.setdefault("FLASK_ENV", FLASK_ENV)
 _DATABASE_PATH = os.path.join(BASE_DIR, "database.db")
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{_DATABASE_PATH}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+###########
+# Postgres
+###########
+sql_password = os.environ.get("DOKU_DB_PASSWORD", "password_doku")
+sql_user = os.environ.get("DOKU_DB_USER", "doku_user")
+sql_host = os.environ.get("DOKU_DB_HOST", "localhost")
+sql_db = os.environ.get("DOKU_DB_DATABASE", "doku_db")
+SQLALCHEMY_DATABASE_URI = f"postgresql://{sql_user}:{sql_password}@{sql_host}/{sql_db}"
