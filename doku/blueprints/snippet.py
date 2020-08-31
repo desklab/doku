562588d203dbf7045fdeb8f83073f5eed8f80c3b
Jonas Drotleff <j.drotleff@desk-lab.de>
2020-08-31 23:49:39 +0200

    Add Snippet overview and basic edit view

---
+++ doku/blueprints/snippet.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template
+
+from doku import db
+from doku.models.snippet import Snippet
+from doku.utils.db import get_pagination_page, get_ordering, get_or_404
+from doku.utils.decorators import login_required
+
+bp = Blueprint("snippet", __name__)
+
+
+@bp.route("/", methods=["GET"])
+@login_required
+def index():
+    page = get_pagination_page()
+    snippets = (
+        db.session.query(Snippet).paginate(page=page, per_page=10)
+    )
+    return render_template("sites/snippets.html", snippets=snippets)
+
+
+@bp.route("/<int:snippet_id>", methods=["GET", "POST"])
+@login_required
+def get(snippet_id: int):
+    snippet = get_or_404(db.session.query(Snippet).filter_by(id=snippet_id))
+    return render_template("sites/snippet_edit.html", snippet=snippet)
