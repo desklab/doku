498f89da88c0eb1b6cfef4a9fc6a8e2aadeb17f7
Jonas Drotleff <j.drotleff@desk-lab.de>
2020-08-31 23:49:09 +0200

    Update models for usage with snippets

---
+++ doku/models/schemas/snippet.py
@@ -0,0 +1,25 @@
+from marshmallow_sqlalchemy import auto_field
+from marshmallow_sqlalchemy.fields import Nested
+
+from doku.models import DateSchemaMixin
+from doku.models.schemas.common import DokuSchema, ApiSchemaMixin
+from doku.models.snippet import Snippet
+
+
+class VariableSchema(DokuSchema, DateSchemaMixin, ApiSchemaMixin):
+    class Meta:
+        model = Snippet
+        load_instance = True
+
+    API_NAME = "snippet"
+
+    id = auto_field()
+    name = auto_field()
+
+    use_markdown = auto_field()
+    css_class = auto_field()
+
+    content = auto_field()
+    compiled_content = auto_field(dump_only=True)
+
+    used_by = Nested("VariableSchema", dump_only=True, only=("name", "id", "document"))
