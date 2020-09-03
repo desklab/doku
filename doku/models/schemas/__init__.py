598839634feeced200219df7d598e23b40333e57
Jonas Drotleff <j.drotleff@desk-lab.de>
2020-09-03 21:23:55 +0200

    Add Snippet Schema with correct class name

--- doku/models/schemas/__init__.py
+++ doku/models/schemas/__init__.py
@@ -1,6 +1,7 @@
 from doku.models.resource import Resource
 from doku.models.schemas.document import DocumentSchema, VariableSchema
 from doku.models.schemas.template import TemplateSchema, StylesheetSchema
+from doku.models.schemas.snippet import SnippetSchema
 
-
-__all__ = [DocumentSchema, VariableSchema, TemplateSchema, StylesheetSchema, Resource]
+__all__ = [DocumentSchema, VariableSchema, TemplateSchema, StylesheetSchema, Resource,
+           SnippetSchema]
