e88f4b0dbf56ff6dc3beededab0fc94738f60e81
Jonas Drotleff <j.drotleff@desk-lab.de>
2020-11-12 17:50:46 +0100

    Make templates a requirement
    Every document should have a template. Also added migrations

--- doku/models/document.py
+++ doku/models/document.py
@@ -13,7 +13,7 @@ class Document(db.Model, DateMixin):
     public = db.Column(db.Boolean)
     author_id = db.Column(db.Integer, db.ForeignKey("doku_user.id"), nullable=False)
     template_id = db.Column(
-        db.Integer, db.ForeignKey("doku_template.id"), nullable=True
+        db.Integer, db.ForeignKey("doku_template.id"), nullable=False
     )
 
     template = db.relationship("Template", back_populates="documents")
