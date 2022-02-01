90077a16247963bb49805f20645128e8575631b6
Jonas Drotleff <j.drotleff@desk-lab.de>
2022-02-01 15:58:32 +0100

    Fix create document

--- doku/signals/document.py
+++ doku/signals/document.py
@@ -6,12 +6,23 @@ from doku.models.base import (
     Template,
     Variable
 )
+from doku.models.template import DEFAULT_TEMPLATE
 from doku.signals import model_created, model_updated
 from doku.utils.db import get_or_create, create_if_not_exists
 
 
 @model_created.connect_via(Document)
-def create_missing_vars(sender: t.Type[db.Model], instance: Document, **extra):
+def create_missing_template_vars(sender: t.Type[db.Model], instance: Document, **extra):
+    if instance.template_id is None:
+        _template = Template(name=f"Template for {instance.name}")
+        _template.source = DEFAULT_TEMPLATE
+        instance.template = _template
+        db.session.add(_template)
+    else:
+        instance.template = (
+            db.session.query(Template).filter_by(id=instance.template_id).one()
+        )
+
     template: Template = instance.template
     for name in template.available_fields:
         variable = Variable(document=instance, name=name)
